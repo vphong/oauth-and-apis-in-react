870ed446e618919c47dd14ae4a2e51c37ba4945a
vphong <vphong@ucsd.edu>
2017-12-28 10:07:24 -0800

    reducers, store

--- src/actions/items.js
+++ src/actions/items.js
@@ -3,16 +3,16 @@
 // STATE ACTIONS
 //////////////////
 
-export function itemHasErrored(bool) {
+export function itemsHasErrored(bool) {
   return {
-    type: 'ITEM_HAS_ERRORED',
+    type: 'ITEMS_HAS_ERRORED',
     hasErrored: bool
   };
 }
 
-export function itemIsLoading(bool) {
+export function itemsIsLoading(bool) {
   return {
-    type: 'ITEM_IS_LOADING',
+    type: 'ITEMS_IS_LOADING',
     isLoading: bool
   };
 }
